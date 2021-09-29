2d8e4455319d82ea1b2e6cef80cd42cf556e7e1b
John Weingart <jrweingart@gmail.com>
2021-09-29 12:13:29 -0400

    Add files via upload

---
+++ Breeze/Breeze/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breeze
+//
+//  Created by John Weingart on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
